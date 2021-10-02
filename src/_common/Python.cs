368d57c9942e76007f5cd503e2fa369a4a6688b6
Dave Skender <dave.skender@gmail.com>
2021-10-02 18:32:52 -0400

    add PythonNet preview package

---
+++ src/_common/Python.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Python.Runtime;
+
+namespace Skender.Stock.Indicators
+{
+    internal class PythonEncoders
+    {
+        // TODO: see if this can be resolved with newer PythonNet packages
+        // in the Python project; would prefer not to mess with it here.
+    }
+}
